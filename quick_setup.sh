4999c48e2ca1910ed5d5b4ed85711a14999a3820
Carlos <carlos.zetina@ivadolabs.com>
2022-02-23 23:25:52 -0500

    Fixed script for windows

--- quick_setup.sh
+++ quick_setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 python -m venv my_env
-source my_env/bin/activate
+if [[ "$OSTYPE" == "msys" ]]; then
+	source ./my_env/Scripts/activate
+else 
+	source my_env/bin/activate
+fi
 pip install --upgrade pip
 pip install -r requirements.txt
 python -m ipykernel install --name=pyomo_tutorial
